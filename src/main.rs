9289ab8772554d06dba72e6a07b148edf5a879bf
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-02-04 19:00:18 +0530

    parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contains it
+#[derive(StructOpt)]
+struct Cli {
+    /// pattern to look for
+    pattern: String,
+    /// the path to the file to read
+    #[structopt(parse(from_os_str))]
+    path:std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
 }
